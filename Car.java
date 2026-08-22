7ec5f896919a0558ac0d9b2d589d495895ec699b
IshaShak <shaikafsheenisha.25.csm@anits.edu.in>
2026-08-22 14:21:23 +0530

    Fix formatting issues in Car.java

--- Car.java
+++ Car.java
@@ -1,13 +1,9 @@
 public  class Car{
     String brand="toyoto";
     String name= "BMW";
-
-
 void display(){
     System.out.println("Brand:"+brand);
     System.out.println("Name:"+ name);
-
-
 }
 public static void main(String[] args) {
     Car obj=new Car();
